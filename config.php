286f448482ffda0332993778bb0bc20a36f61c97
mamazaki <mamazakikasookie@gmail.com>
2026-05-26 15:46:03 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// ไฟล์นี้จะถูกแก้ไขอัตโนมัติเมื่อกดบันทึกจากหน้า admin.php
+$files = [
+    "file_1" => [
+        "name" => "คู่มือการใช้งานระบบ WFH สำนักงานศึกษาธิการจังหวัด",
+        "url" => "https://example.com/files/wfh_manual.pdf",
+        "category" => "cat_1",
+        "owner" => "admin"
+    ]
+];
